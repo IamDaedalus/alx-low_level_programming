59bc30eab6c281c5bb584a629467c893efb85718
Daedalus <manny.is.daedalus@pm.me>
2023-02-16 15:43:15 -0800

    are you happy betty?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	char my_str[] = "\"Programming is like building a multilingual puzzle";
