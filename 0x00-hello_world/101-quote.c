bf4f665f78518a6c8788aa9309163d2fe83a32b5
Daedalus <manny.is.daedalus@pm.me>
2023-02-16 18:02:11 -0800

    redo of the whole thing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 
 /**
-   * main - Entry point
-   *
-   * Return: 1
-   */
+ * main - Entry point
+ *
+ * Return: 1
+*/
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(char), sizeof(str), stderr);
-
+	write(1, str, strlen(str) - 1);
 	return (1);
 }
+
