5e896a7d9aeef2725f34b927c0a4cdff1ffcadde
Daedalus <manny.is.daedalus@pm.me>
2023-02-17 17:50:23 -0800

    i'm all out of wit now. i'll get back when I wake

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	x = '0';
+	while (x <= '9')
+	{
+		putchar(x);
+
+		if (x < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
