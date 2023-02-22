866129e59e6d8599a6efd889cae894a01c96f6f6
Daedalus <manny.is.daedalus@pm.me>
2023-02-21 19:17:03 -0800

    absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _abs - return the absolute value of a number
+  * @n: the value to be operated on
+  * Return: returns the valuen i.e the abs value
+  */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return n;
+	}
+}
