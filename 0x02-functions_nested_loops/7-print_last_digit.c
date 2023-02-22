adc0caff912c8b3f426b10f15a50aa79eea87159
Daedalus <manny.is.daedalus@pm.me>
2023-02-21 19:51:32 -0800

    8044

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_last_digit - this prints the last digit of a given number
+  * @n: the incoming number
+  * Return: returns the last digit in a number
+  */
+int print_last_digit(int n)
+{
+	int k;
+
+	if (n < 0)
+	{
+		k = (-1 * n) % 10;
+	}
+	else
+	{
+		k = n % 10;
+	}
+	_putchar(k + '0');
+	return (k);
+}
