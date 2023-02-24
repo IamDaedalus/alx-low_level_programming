7be6011a0624c07401e37dfd5e73afd58b2f543a
Daedalus <manny.is.daedalus@pm.me>
2023-02-23 20:24:52 -0800

    together in union

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - adds two numbers
+  * @m: first number
+  * @n: second number
+  * Return: the result of the two numbers added
+  */
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+
+	return (result);
+}
