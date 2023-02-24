5f2ee6d6dcfe92ce5e617a723c5eac511c1d8cca
Daedalus <manny.is.daedalus@pm.me>
2023-02-24 15:06:34 -0800

    who's the largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,16 +7,15 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if ((a >= b && b >= c) || (a >= c && c >= b))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b >= a && a >= c) || (b >= c &&  c >= a))
 	{
 		largest = b;
 	}
@@ -24,6 +23,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
