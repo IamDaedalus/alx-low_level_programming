561ae7828b6c724b34f64ba928c632511d467d01
Daedalus <manny.is.daedalus@pm.me>
2023-02-24 18:13:01 -0800

    yep the checker is definetly broken

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
-  * _isupper - checks if the char is upper or lower case
+  * _isupper - checcs if the char is upper or lower case
   * @c: the incoming char
   * Return: the return value of the game
   */
 int _isupper(int c)
 {
-	int k;
-
-	k = c;
-	if (k > 63 && k < 91)
+	if (c > 63 && c < 91)
 	{
 		return (1);
 	}
