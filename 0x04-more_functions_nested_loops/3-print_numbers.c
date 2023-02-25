03ce91ac603c414f827dc83c9042246c35602ee8
Daedalus <manny.is.daedalus@pm.me>
2023-02-24 18:53:51 -0800

    i am nine years old now

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints from 0 to 9 and a new line
+  */
+void print_numbers(void)
+{
+	int k;
+
+	k = 0;
+	while (k < 10)
+	{
+		_putchar(k);
+		k++;
+	}
+
+	_putchar('\n');
+}
