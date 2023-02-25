9c5d91ad5502af7c97bccf58986f80b9dd2517c6
Daedalus <manny.is.daedalus@pm.me>
2023-02-24 18:42:28 -0800

    together we go higher

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * mul - multiplies any two given numbers together
+  * @m: number one
+  * @n: number two
+  * Return: returns the result of multiplication
+  */
+int mul(int m, int n)
+{
+	return (m * n);
+}
