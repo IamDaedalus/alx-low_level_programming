2a6075a1863be7b2ee5ffae3132cbfd8c23dceec
Daedalus <manny.is.daedalus@pm.me>
2023-02-28 20:01:44 -0800

    local tests broken, once again all up to the checker

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * puts2 - prints every other char in the string
+  * @str: the incoming string
+  */
+void puts2(char *str)
+{
+	int length = 0, index = 0;
+
+	while (str[index])
+		length++;
+
+	for (index = 0; index < length; index += 2)
+		_putchar(*(str + index));
+
+	_putchar('\n');
+}
