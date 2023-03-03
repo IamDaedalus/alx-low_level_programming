7dc3c4aaaeeab9bb592b6795943a25a252816f21
Daedalus <manny.is.daedalus@pm.me>
2023-03-02 21:46:31 -0800

    l337 c0d3 0r s0m37h1ng

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * leet - turns the input into leet
+  * @char: the incoming text
+  * Return: returns the leet code
+  */
+char *leet(char *str)
+{
+	int i = 0, k;
+
+	char key[] = "aAeEoOtTlL";
+	char map[] = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			if(key[k] == str[i])
+			{
+				str[i] = map[k];
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
