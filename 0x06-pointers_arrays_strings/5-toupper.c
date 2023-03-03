a77ae22c54edbbcc8f8a8839ffedd693a94cddf4
Daedalus <manny.is.daedalus@pm.me>
2023-03-02 21:28:11 -0800

    SHOUT

---
+++ 0x06-pointers_arrays_strings/5-toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * string_toupper - this converts a str to uppercase
+  * @str: the string to convert
+  * Return: return the string converted to uppercase
+  */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
