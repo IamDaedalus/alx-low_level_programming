9dd4902eb73d97be0fdb47a36ed1dabe5d03c520
Daedalus <manny.is.daedalus@pm.me>
2023-03-06 04:28:34 -0800

    les check again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,11 @@
 #include "main.c"
 
+/**
+  * _strchr - finds the first occurence of a char in a str
+  * @s: the str to check
+  * @b: the character to check
+  * Return: returns the char if found or null if not
+  */
 char *_strchr(char *s, char b)
 {
 	while (*s != '\0')
