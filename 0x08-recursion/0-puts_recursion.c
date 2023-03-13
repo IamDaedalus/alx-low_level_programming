0f5a591292b702cd9c3766ebebe60bebc4d8d130
Daedalus <manny.is.daedalus@pm.me>
2023-03-12 23:14:29 -0700

    pc's working back and better

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
   */
 void _puts_recursion(char *s)
 {
-	while (*s)
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
-		s++;
+		/* the line below is similar to the resource video's */
+		_puts_recursion(s++);
 	}
-
-	_putchar('\n');
 }
