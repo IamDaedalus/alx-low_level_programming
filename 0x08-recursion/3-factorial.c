ac0084e708f8745443de8c0d158af7502dff3626
Daedalus <manny.is.daedalus@pm.me>
2023-03-14 00:52:19 -0700

    i noticed the cheeky inception one liners Mr Barbier

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * factorial - prints the factorial of a number
+  * @n: the number
+  * Return: returns the result of the factorial
+  */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+
+	/* n is reduced every call to the function which makes it work */
+	return n * factorial(n - 1);
+}
