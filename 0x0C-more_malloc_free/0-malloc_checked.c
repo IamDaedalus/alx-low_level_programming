9919e34973ff901980fa2e1b73ff1ae1282cdfc8
Daedalus <manny.is.daedalus@pm.me>
2023-03-17 17:44:46 -0700

    failed or not failed? let's tune in to what the checker says!

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+  * malloc_checked - assigns a specified amount of memory
+  *			to a ptr using malloc
+  * @b: the amount of memory to allocate
+  * Return: returns a successful pointer to the memory or further
+  *			processing using the exit() function
+  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
+
 	ptr = malloc(b);
 
-	if (ptr != NULL)
-		return ptr;
+	if (ptr == NULL)
+		exit(98);
 	else
-		exit (98);
+		return (ptr);
 }
