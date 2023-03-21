e5d8db429dae574038d1366bcac25ee547cef864
Daedalus <manny.is.daedalus@pm.me>
2023-03-20 22:56:54 -0700

    poppy has owners ahhhhhhh :))))

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,14 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+  * print_dog - prints the details of a struct
+  * @d: the struct to print details from
+  */
 void print_dog(struct dog *d)
 {
 	char *n, *o;
+
 	if (d != NULL)
 	{
 		n = d->name == NULL ? "(nil)" : d->name;
