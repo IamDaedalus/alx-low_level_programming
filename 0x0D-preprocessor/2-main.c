ff2e14a2b0662d019326370ebf182ee6631ce979
Daedalus <manny.is.daedalus@pm.me>
2023-03-24 01:47:49 -0700

    added the task #2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - entry point of every C program
+  * Return: returns 0 for success
+  */
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
