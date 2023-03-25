8dbb1121865ad1a4bed478440cd3f54d7fca259a
Daedalus <manny.is.daedalus@pm.me>
2023-03-24 22:53:33 -0700

    made fixes for task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+  * int_index - returns the index of an array tht returns the first
+  * value that cmp returns >0
+  * @array: the array of integers to check
+  * @size: size of the array
+  * @cmp: the function that is used to compare the values
+  * Return: returns the index of the element that cmp returns > 0
+  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
