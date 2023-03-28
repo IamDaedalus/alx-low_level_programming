dc67ea7e2c53da6025c85b1c270796193b7cc94c
Daedalus <manny.is.daedalus@pm.me>
2023-03-28 01:34:24 -0700

    cleanup for task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,34 +8,38 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	char *s;
-	int i;
-	double d;
+	char *str;
+	unsigned int i = 0;
+	int flag;
 
 	va_start(args, format);
-	while (format && *format)
+	while (format && format[i])
 	{
-		switch (*format++)
+		flag = 1;
+		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int);
+				printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, int));
+				printf("%f", (float)va_arg(args, double));
 				break;
 			case 's':
-				if (s == NULL)
-					printf("(nil)");
-				printf("%s", va_arg(args, int));
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
 				break;
 			default:
-				continue;
+				flag = 0;
+				break;
 		}
-		if (*format)
+		if (flag && format[i + 1])
 			printf(", ");
+		i++;
 	}
 	printf("\n");
 	va_end(args);
