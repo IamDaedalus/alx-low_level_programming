039c3e0b147686cbca45df917ec40e7d128189a5
Emmanuel Quansah <manny.is.daedalus@pm.me>
2023-04-01 20:45:56 -0700

    fix for advanced task 1

--- 0x12-singly_linked_lists/101-first.c
+++ 0x12-singly_linked_lists/101-first.c
@@ -1,13 +1,11 @@
-#include "lists.h"
 #include <stdio.h>
 
-void __attribute__((__constructor__)) get_rekt(void);
 
 /**
   * get_rekt - this code executes before main
   * @void: returns nothing
   */
-void get_rekt(void)
+void __attribute__((__constructor__)) get_rekt(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
