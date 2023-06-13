c2e955b4f4e8a39dd123e9fc35d3b94089ee9fd3
Daedalus <manny.is.daedalus@pm.me>
2023-06-13 19:14:51 +0000

    finished advanced task one

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,30 +1,77 @@
 #include "main.h"
+#include <inttypes.h>
 #include <stdlib.h>
 
+/**
+ * calc_args_len - calculates the length of each args
+ * @ac: the total number of args
+ * @av: the argv
+ * Return: returns the length of each args
+ */
+int calc_args_len(int ac, char **av)
+{
+	int args_len = 0;
+	int i, j;
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			args_len++;
+			j++;
+		}
+		args_len++;  /* Increment here to account for the '\n' character */
+	}
+	return (args_len);
+}
+
+/**
+ * concat_args - concats each arg to str
+ * @ac: the argc
+ * @av: the argv
+ * @str: the string to concat in
+ */
+void concat_args(int ac, char **av, char *str)
+{
+	int i, j;
+	int str_pos = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			str[str_pos] = av[i][j];
+			str_pos++;
+			j++;
+		}
+		if (str[str_pos] == '\0')
+			str[str_pos++] = '\n';
+	}
+}
+
+/**
+ * argstostr - prints each argument as seperate string
+ * @ac: the args count
+ * @av: the args vector
+ * Return: one big string newline seperated based on args
+ */
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int argc, argvc, i = 0;
-	int args_len = 0;
+	int result_len;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	result_len = calc_args_len(ac, av);
 
+	str = malloc(sizeof(char) * (result_len + 1));
+	if (!str)
+		return (NULL);
 
-	argc = 0;
-	while (argc < ac)
-	{
-		argvc = 0;
-		while (av[argc][argvc] != '\0')
-		{
-			str[i] = av[argc][argvc];
-			argvc++;
-		}
-		str[i] = '\n';
-		argc++;
-	}
-
+	concat_args(ac, av, str);
 
 	return (str);
 }
