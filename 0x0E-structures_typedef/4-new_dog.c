d118b8ffd2092f726099a948b32cf22707e2b378
Daedalus <manny.is.daedalus@pm.me>
2023-06-20 07:32:51 +0000

    fixed errors. submitting for checking

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,61 @@
 #include "dog.h"
 #include <stdlib.h>
 
+char *dupstr(char *str)
+{
+	char *result;
+	int len = 0, i = 0;
+
+	if (str)
+	{
+		while (str[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+
+		result = malloc(sizeof(char) * len + 1);
+
+		i = 0;
+		while (str[i] != '\0')
+		{
+			result[i] = str[i];
+			i++;
+		}
+	}
+	else
+		return (NULL);
+
+	result[i] = '\0';
+	return (result);
+}
+
+
 /**
  * new_dog - creates a new dog_t struct
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
- * Return: returns a new dog_t struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *n, *o;
 	dog_t *result;
 
-	result = malloc(sizeof(dog_t));
+	n = !name ? NULL : dupstr(name);
+	o = !owner ? NULL : dupstr(owner);
 
-	if (result == NULL)
+	if (!n || !o)
 		return (NULL);
 
-	result->name = name;
+	result = malloc(sizeof(dog_t));
+	if (!result)
+		return (NULL);
+
+	result->name = n;
 	result->age = age;
-	result->owner = owner;
+	result->owner = o;
+
 
 	return (result);
 }
