150b01c63315f0de3bc14ded31fd846aa10b65d5
Daedalus <manny.is.daedalus@pm.me>
2023-09-08 16:07:05 +0000

    checking task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a new hash table of size "size"
+ * @size: the size of the hash table
+ * Return: returns the address of the tabele we just created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+
+	if (!new)
+		return (NULL);
+
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (!new->array)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
+	return (new);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+This is the ALX Hash Table and Functions project
+by Manny
